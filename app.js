065db1526860a609a550085c0606b8b4df72b856
Nikhil Sai <venkatasainikhil20@gmail.com>
2025-09-25 15:49:39 +0530

    Update greeting message for testing

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 const port = 8080;
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Dockerized Jenkins Pipeline!!');
+  res.end('Hello from Dockerized Jenkins Pipeline!');
 });
 server.listen(port);
